42592b831b5536b429472c10563054fb4a11e9ca
JungaJK <wjddk0929@gmail.com>
2018-05-14 09:29:04 +0100

    add new entities

--- LibraryManagement.Data/LibraryContext.cs
+++ LibraryManagement.Data/LibraryContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LibraryManagement.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace LibraryManagement.Data
@@ -7,6 +8,16 @@ namespace LibraryManagement.Data
     {
         public LibraryContext(DbContextOptions options) : base(options) { }
 
-        public DbSet<Patron> Patrons { get; set; }
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Video> Videos { get; set; }
+        public virtual DbSet<Checkout> Checkouts { get; set; }
+        public virtual DbSet<CheckoutHistory> CheckoutHistories { get; set; }
+        public virtual DbSet<LibraryBranch> LibraryBranches { get; set; }
+        public virtual DbSet<BranchHours> BranchHours { get; set; }
+        public virtual DbSet<LibraryCard> LibraryCards { get; set; }
+        public virtual DbSet<Patron> Patrons { get; set; }
+        public virtual DbSet<Status> Statuses { get; set; }
+        public virtual DbSet<LibraryAsset> LibraryAssets { get; set; }
+        public virtual DbSet<Hold> Holds { get; set; }
     }
 }
---
+++ LibraryManagement.Data/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LibraryManagement.Data.Models
+{
+    public class Book : LibraryAsset
+    {
+        [Required]
+        public string IBAN { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string  DeweyInbox { get; set; }
+    }
+}
