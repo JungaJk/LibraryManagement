e21be270eede91b22602d43ac20a33f43987d2cb
JungaJK <wjddk0929@gmail.com>
2018-05-14 09:42:37 +0100

    db migration

--- LibraryManagement.Data/Migrations/20180514083634_add initial entity models.Designer.cs
+++ LibraryManagement.Data/Migrations/20180514083634_add initial entity models.Designer.cs
@@ -4,14 +4,16 @@ using LibraryManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LibraryManagement.Data.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20180514083634_add initial entity models")]
+    partial class addinitialentitymodels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
